0609f83e93fd32a3e48a6a7ae9a628fbe3fdf796
EasonC13 <eason.tw.chen@gmail.com>
2024-11-05 01:27:09 -0500

    update

--- src/constants/navList.ts
+++ src/constants/navList.ts
@@ -5,6 +5,12 @@ export const NAV_LIST = [
     path: "/",
     newTab: false,
   },
+  {
+    id: "Math Feedback Demo",
+    name: "Math Feedback Demo",
+    path: "https://math.gptutor.tools/class/cm1tf3i690000sypn2qe7w81b/homework/cm1tf84xa0001sypnkcz1f4sa/cm342h1mu0000e53vedk0jl2b?vtutor=true&classId=cm1tf3i690000sypn2qe7w81b&homeworkProblemId=cm1tf84xa0001sypnkcz1f4sa&homeworkSubmissionId=cm1v5cfku0000i14o98prfv27",
+    newTab: true,
+  },
   // {
   //   id: "API Docs",
   //   name: "API Docs",
