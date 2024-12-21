07e45f63160f557633c5d56e7ebb8cf261996f4c
EasonC13 <eason.tw.chen@gmail.com>
2024-12-21 09:17:46 +0800

    update region specify

--- src/pages/api/openai/chat.ts
+++ src/pages/api/openai/chat.ts
@@ -7,6 +7,26 @@ import { getChatPrompt } from "@/prompt/getChatPrompt";
 
 export const config = {
   runtime: "edge",
+  preferredRegion: [
+    "arn1",
+    "bom1",
+    "cdg1",
+    "cle1",
+    "cpt1",
+    "dub1",
+    "fra1",
+    "gru1",
+    // "hkg1",
+    "hnd1",
+    "iad1",
+    "icn1",
+    "kix1",
+    "lhr1",
+    "pdx1",
+    "sfo1",
+    "sin1",
+    "syd1",
+  ],
 };
 
 export default async function handler(req: NextRequest) {
